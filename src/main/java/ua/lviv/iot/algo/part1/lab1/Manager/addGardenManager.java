63edd3a315ef56dfe6c1563ddcd16d081d49562a
IgorIoT13 <ihor.slodziak.ir.2022@lpnu.ua>
2023-04-10 10:32:31 +0300

    Fix lab

--- src/main/java/ua/lviv/iot/algo/part1/lab1/Manager/addGardenManager.java
+++
@@ -1,7 +0,0 @@
-package ua.lviv.iot.algo.part1.lab1.Manager;
-
-import ua.lviv.iot.algo.part1.lab1.Garden;
-
-public abstract class addGardenManager {
-   public abstract void addObject(Garden garden);
-}
